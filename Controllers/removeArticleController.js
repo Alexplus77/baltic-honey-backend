86e58eb1e9c242147e96eb8106c291c66e5463dc
alexs <alexplus77@mail.ru>
2022-08-02 11:30:52 +0200

    baggg

--- Controllers/removeArticleController.js
+++
@@ -1,7 +0,0 @@
-const Articles = require("../Models/articleModel");
-//Контроллер удаляет статью в БД
-exports.removeArticle = (req, res) => {
-  Articles.findByIdAndRemove(req.body.id, {}, (err, doc) => {
-    doc && res.send("ok");
-  });
-};
